29a0c25bd433ad6239de1b5ee425b601ea12866f
pshenhao <pshenhao@qq.com>
2024-07-12 09:50:17 +0800

    upload code

--- scripts/quantize_eval_llama.sh
+++ scripts/quantize_eval_llama.sh
@@ -1,5 +1,3 @@
 # python examples/quantize_eval.py --model /home/wf/models/Llama2-Chinese-13b-Chat --quant-config ./configs/gptq_cmmlu_hm_quant_config.yaml --eval-ppl --eval-ceval --eval-cmmlu --eval-boss
 # python examples/quantize_eval.py --model /home/wf/models/Llama2-Chinese-13b-Chat --quant-config ./configs/gptq_cmmlu_ss_quant_config.yaml --eval-ppl --eval-ceval --eval-cmmlu --eval-boss
 # python examples/quantize_eval.py --model /home/wf/models/Llama2-Chinese-13b-Chat --quant-config ./configs/gptq_cmmlu_st_quant_config.yaml --eval-ppl --eval-ceval --eval-cmmlu --eval-boss
-
-python examples/quantize_eval.py --model /home/wf/models/Llama2-Chinese-13b-Chat --quant-config ./configs/gptq_cmmlu_hm_quant_config.yaml --save 'llama2-13b-chat-gptq-4bit.pth'
